b33560507b4aabd1b71861eb26b1563f8bba4ec9
tce96 <tce96sj@gmail.com>
2026-02-24 20:33:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/pearl-jubilee-hub-connect/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
