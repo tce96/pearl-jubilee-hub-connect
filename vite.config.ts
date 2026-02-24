b2b16763dd3fd9face39cfa62ac14d3eefeb9b97
tce96 <tce96sj@gmail.com>
2026-02-24 19:53:05 +0530

    Update vite.config.ts
    Modified base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -12,6 +16,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/pearl-jubilee-hub-connect/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
